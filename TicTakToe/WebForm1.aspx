0792c2e2aaf4032b51093e78aee8209945c69a88
Bandana Mahapatra <bmahapatra@tavisca.com>
2017-08-31 18:37:57 +0530

    TikTakToe in asp.net

---
+++ TicTakToe/WebForm1.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="TicTakToe.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+        <asp:Button ID="A1" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text="turn" />
+        <asp:Button ID="A2" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text="turn" />
+        <asp:Button ID="A3" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text="turn" />
+       </p>
+            <p>
+            <asp:Button ID="B1" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text="turn" />
+            <asp:Button ID="B2" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text="turn" />
+            <asp:Button ID="B3" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text="turn" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;<asp:Label ID="Label1" runat="server" Height="32px" SkinID="50" Width="132px" BorderWidth="5px"></asp:Label>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </p>
+        <p>
+            <asp:Button ID="C1" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text="turn" />
+            <asp:Button ID="C2" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text="turn" />
+            <asp:Button ID="C3" runat="server" BorderStyle="Solid" BorderWidth="5px" Height="88px" OnClick="ButtonClick" Width="100px" BackColor="#3399FF" Text="turn" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </p>
+        <p>
+            &nbsp;</p>
+        <div style="margin-left: 80px">
+&nbsp;&nbsp;
+            <asp:Button ID="Restart" runat="server" BorderWidth="5px" OnClick="Restart_Click" Text="Rename" Width="148px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ TicTakToe/WebForm1.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TicTakToe
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        public static int player = 2;
+        public int s1=0;
+        public int s2 = 0;
+        public int sd = 0;
+
+        protected void Page_Load(object OBJ, EventArgs e)
+        {
+        }
+
+        private void disableButtons()
+        {
+            A1.Enabled = A2.Enabled = A3.Enabled = B1.Enabled = B2.Enabled = B3.Enabled = C1.Enabled = C2.Enabled = C3.Enabled = false;
+        }
+        protected void ButtonClick(object sender, EventArgs e)
+        {
+            Button button =(Button) sender;
+
+            if (button.Text == "turn")
+            {
+                if (player % 2 == 0)
+                {
+                    button.Text = "X";
+                    if(Winner()== true)
+                    {
+                        Label1.Text = "X is the winner";
+                        disableButtons();
+                    }
+                    player++;
+                }
+                else
+                {
+                    button.Text = "O";
+                    if (Winner() == true)
+                    {
+                        Label1.Text = "O is the winner";
+                        disableButtons();
+                    }
+                    player++;
+                }
+                if(Draw()==true)
+                {
+                    Label1.Text = "Match is Draw.!!!!";
+                }
+            }
+        }
+
+        protected bool Winner()
+        {
+            if(A1.Text== A2.Text && A2.Text==A3.Text && A1.Text!="turn")
+            {
+                return true;
+            }
+            if (B1.Text == B2.Text && B2.Text == B3.Text && B1.Text != "turn")
+            {
+                return true;
+            }
+            if (C1.Text == C2.Text && C2.Text == C3.Text && C1.Text != "turn")
+            {
+                return true;
+            }
+            if (A1.Text == B1.Text && B1.Text == C1.Text && A1.Text != "turn")
+            {
+                return true;
+            }
+            if (A2.Text == B2.Text && B2.Text == C2.Text && A2.Text != "turn")
+            {
+                return true;
+            }
+            if (A3.Text == B3.Text && B3.Text == C3.Text && A3.Text != "turn")
+            {
+                return true;
+            }
+            if (A1.Text == B2.Text && B2.Text == C3.Text && A1.Text != "turn")
+            {
+                return true;
+            }
+            if (A3.Text == B2.Text && B2.Text == C1.Text && A3.Text != "turn")
+            {
+                return true;
+            }
+
+           
+          return false;
+
+        }
+
+        public bool Draw()
+        {
+            if(A1.Text!="turn" && A2.Text != "turn" && A3.Text != "turn" && B1.Text != "turn" && B2.Text != "turn" && B3.Text != "turn" && C1.Text != "turn" && C2.Text != "turn" && C3.Text != "turn" )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        protected void Restart_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("WebForm1.aspx");
+        }
+    }
+
+        
+    }
